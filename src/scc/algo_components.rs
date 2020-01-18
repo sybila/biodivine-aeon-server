8bc77824ec872be40f0b0b17040955230efd4c7d
Samuel Pastva <daemontus@gmail.com>
2020-01-18 17:46:20 +0100

    better parallel approach

--- src/scc/algo_components.rs
+++ src/scc/algo_components.rs
@@ -4,7 +4,7 @@ use biodivine_lib_param_bn::bdd_params::BddParams;
 use biodivine_lib_std::param_graph::{EvolutionOperator, Graph, Params};
 use biodivine_lib_std::IdState;
 use std::option::Option::Some;
-use crate::scc::algo_par_reach::guarded_reach;
+use crate::scc::algo_par_reach::{guarded_reach, guarded_reach_fwd, guarded_reach_bwd};
 
 pub fn components<F>(graph: &AsyncGraph, mut on_component: F)
 where
@@ -14,7 +14,8 @@ where
     let fwd = graph.fwd();
     let bwd = graph.bwd();
     // TODO: There is a bug if we are not detecting sinks, but maybe its in classifier...
-    /*let mut sinks = StateSet::new(num_states);
+    let initial = StateSet::new_with_fun(num_states, |_| Some(graph.unit_params().clone()));
+    let mut sinks = StateSet::new(num_states);
     for s in graph.states() {
         let has_next = fwd
             .step(s)
@@ -24,7 +25,7 @@ where
             sinks.put(s, is_sink);
         }
     }
-    let can_reach_sink = reach(&bwd, &sinks);
+    let can_reach_sink = guarded_reach_bwd(graph, &sinks, &initial);
     on_component(sinks); // notify about the sinks we have found
     let initial = StateSet::new_with_fun(num_states, |i| {
         if let Some(sink) = can_reach_sink.get(i) {
@@ -32,8 +33,7 @@ where
         } else {
             Some(graph.unit_params().clone())
         }
-    });*/
-    let initial = StateSet::new_with_fun(num_states, |_| Some(graph.unit_params().clone()));
+    });
 
     if initial.iter().next() == None {
         return;
@@ -50,8 +50,8 @@ where
         );
         let pivots = find_pivots(graph, &universe);
         println!("Pivots state count: {}", pivots.iter().count());
-        let forward = guarded_reach(&fwd, &pivots, &universe);
-        let component_with_pivots = guarded_reach(&bwd, &pivots, &forward);
+        let forward = guarded_reach_fwd(graph, &pivots, &universe);
+        let component_with_pivots = guarded_reach_bwd(graph, &pivots, &forward);
         let reachable_terminals = forward.minus(&component_with_pivots);
 
         let leaves_current = reachable_terminals
@@ -68,7 +68,7 @@ where
             on_component(terminal);
         }
 
-        let basins_of_reachable_terminals = guarded_reach(&bwd, &forward, &universe);
+        let basins_of_reachable_terminals = guarded_reach_bwd(graph, &forward, &universe);
         let empty = graph.empty_params();
         let unreachable_terminals = StateSet::new_with_fun(num_states, |s| {
             let in_basin = basins_of_reachable_terminals.get(s).unwrap_or(&empty);
--- src/scc/algo_par_reach.rs
+++ src/scc/algo_par_reach.rs
@@ -6,6 +6,94 @@ use biodivine_lib_std::IdState;
 use rayon::prelude::*;
 use std::collections::{HashMap, HashSet};
 
+pub fn guarded_reach_fwd(graph: &AsyncGraph, initial: &StateSet, guard: &StateSet) -> StateSet {
+    return guarded_reach(&graph.fwd(), &graph.bwd(), initial, guard);
+}
+
+pub fn guarded_reach_bwd(graph: &AsyncGraph, initial: &StateSet, guard: &StateSet) -> StateSet {
+    return guarded_reach(&graph.bwd(), &graph.fwd(), initial, guard);
+}
+
+pub fn guarded_reach<F, B>(fwd: &F, bwd: &B, initial: &StateSet, guard: &StateSet) -> StateSet
+    where
+        F: EvolutionOperator<State=IdState, Params=BddParams> + Send + Sync,
+        B: EvolutionOperator<State=IdState, Params=BddParams> + Send + Sync
+{
+    let capacity = initial.capacity();
+    let mut result_set = StateSet::new(capacity);
+    let mut changed = HashSet::new();
+    for (s, p) in initial.iter() {
+        result_set.put(s, p.clone());
+        changed.insert(s);
+    }
+
+    while !changed.is_empty() {
+        println!("Wave size: {}", changed.len());
+
+        // All successors of changed states
+        let recompute: HashSet<IdState> = changed
+            .par_iter()
+            .flat_map(|s| {
+                fwd.step(*s).map(|(t, p)| t).collect::<Vec<_>>()
+            })
+            .collect();
+
+        let update: Vec<(IdState, BddParams)> = recompute
+            .par_iter()
+            .filter_map(|t| {
+                let guard_t = guard.get(*t);
+                if let Some(guard_t) = guard_t {
+                    let add_to_t: Option<BddParams> = bwd.step(*t).map(|(s, edge)| {
+                        if changed.contains(&s) {
+                            let current_s = result_set.get(s);
+                            if let Some(current_s) = current_s {
+                                let s_adds_to_t = current_s.intersect(&edge).intersect(guard_t);
+                                if !s_adds_to_t.is_empty() {
+                                    Some(s_adds_to_t)
+                                } else {    // empy sets are pointless
+                                    None
+                                }
+                            } else {    // if current_s is empty, s won't contribute anything
+                                None
+                            }
+                        } else {    // if s was not changed, it contributes nothing
+                            None
+                        }
+                    }).fold(None, |a, b| {
+                        match (a, b) {
+                            (Some(a), Some(b)) => Some(a.union(&b)),
+                            (Some(a), None) => Some(a),
+                            (None, Some(b)) => Some(b),
+                            (None, None) => None
+                        }
+                    });
+                    let current_t = result_set.get(*t);
+                    let add_to_t = if let Some(current_t) = current_t {
+                        // if there already is something in t, we have to check if we are adding something new
+                        add_to_t
+                            .filter(|p| !p.is_subset(current_t))
+                            .map(|p| p.union(current_t))
+                    } else {
+                        add_to_t
+                    };
+                    add_to_t.map(|p| (*t, p))
+                } else {    // guard is empty... no need to compute anything
+                    None
+                }
+            })
+            .collect();
+
+        changed.clear();
+        for (s, p) in update {
+            changed.insert(s);
+            result_set.put(s, p);
+        }
+    }
+
+
+    return result_set;
+}
+
 /// Parallel reach procedure works in waves:
 ///  - Each wave has an input of `changed` states - these are the states where the value
 ///  was changed in the last wave (or initial states for the first wave).
@@ -14,7 +102,7 @@ use std::collections::{HashMap, HashSet};
 ///  - Finally, we fold-reduce these into a new state set of `changed` states.
 ///  - In the end, we can (sequentially) put these into the result state set and create
 ///  the set of `changed` states for the next wave.
-pub fn guarded_reach<G>(fwd: &G, initial: &StateSet, guard: &StateSet) -> StateSet
+pub fn guarded_reach_old<G>(fwd: &G, initial: &StateSet, guard: &StateSet) -> StateSet
     where G: EvolutionOperator<State=IdState, Params=BddParams> + Send + Sync
 {
     let capacity = initial.capacity();
