31b1a5731ef664c5170ccccb9b148557a14dd0a8
Samuel Pastva <daemontus@gmail.com>
2020-01-18 18:41:27 +0100

    fix quotes in class display trait

--- src/scc/impl_class.rs
+++ src/scc/impl_class.rs
@@ -14,6 +14,6 @@ impl Class {
 
 impl Display for Class {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
-        return write!(f, "\"{:?}\"", self.0);
+        return write!(f, "{:?}", self.0.iter().map(|c| format!("{:?}", c)).collect::<Vec<_>>());
     }
 }
