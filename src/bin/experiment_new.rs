ec6dac37c5b933fd81a69aa47661b4e2d98cf2b5
Samuel Pastva <sam.pastva@gmail.com>
2022-05-20 15:28:46 +0200

    Add input inlining to experiment.

--- src/bin/experiment_new.rs
+++ src/bin/experiment_new.rs
@@ -1,5 +1,5 @@
 use biodivine_lib_param_bn::symbolic_async_graph::SymbolicAsyncGraph;
-use biodivine_lib_param_bn::BooleanNetwork;
+use biodivine_lib_param_bn::{BooleanNetwork, RegulatoryGraph};
 use std::convert::TryFrom;
 use std::io::Read;
 use biodivine_aeon_server::algorithms::attractors::transition_guided_reduction;
@@ -10,12 +10,8 @@ async fn main() {
     std::io::stdin().read_to_string(&mut buffer).unwrap();
 
     let model = BooleanNetwork::try_from(buffer.as_str()).unwrap();
-    let names: Vec<_> = model
-        .variables()
-        .map(|id| model.get_variable_name(id))
-        .collect();
-    println!("Model loaded...");
-    println!("{} variables: {:?}", model.num_vars(), names);
+    let model = inline_inputs(model);
+    println!("Model loaded. {} variables and {} parameters.", model.num_vars(), model.num_parameters());
 
     let graph = SymbolicAsyncGraph::new(model).unwrap();
 
@@ -33,4 +29,46 @@ async fn main() {
     let universe = transition_guided_reduction(&graph, num_cpus::get_physical()).await;
 
     println!("Universe: {}", universe.approx_cardinality());
+}
+
+fn inline_inputs(bn: BooleanNetwork) -> BooleanNetwork {
+    let mut variables = Vec::new();
+    let mut parameters = Vec::new();
+    for var in bn.variables() {
+        if bn.regulators(var).len() == 0 {
+            parameters.push(bn.get_variable_name(var).clone());
+        } else {
+            variables.push(bn.get_variable_name(var).clone());
+        }
+    }
+
+    let mut inlined_rg = RegulatoryGraph::new(variables.clone());
+
+    for reg in bn.as_graph().regulations() {
+        let old_regulator = bn.get_variable_name(reg.get_regulator());
+        let old_target = bn.get_variable_name(reg.get_target());
+        if variables.contains(old_regulator) {
+            inlined_rg.add_regulation(
+                old_regulator,
+                old_target,
+                false,
+                reg.get_monotonicity()
+            ).unwrap();
+        }
+    }
+
+    let mut inlined_bn = BooleanNetwork::new(inlined_rg);
+
+    for param in parameters {
+        inlined_bn.add_parameter(param.as_str(), 0).unwrap();
+    }
+
+    for var in inlined_bn.variables() {
+        let name = inlined_bn.get_variable_name(var).clone();
+        let old_id = bn.as_graph().find_variable(name.as_str()).unwrap();
+        let old_function = bn.get_update_function(old_id).as_ref().unwrap();
+        inlined_bn.add_string_update_function(name.as_str(), old_function.to_string(&bn).as_str()).unwrap();
+    }
+
+    inlined_bn
 }
